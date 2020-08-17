bd1014ddf1806af61a545002ac5483500a099412
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-08-17 04:02:46 -0400

    ...

--- swag-tag-ui5-catalog.ts
+++ swag-tag-ui5-catalog.ts
@@ -1,8 +1,10 @@
 import { XtalElement, define, symbolize, p, TransformValueOptions, AttributeProps, SelectiveUpdate, Plugins, RenderContext } from 'xtal-element/XtalElement.js';
 import { createTemplate } from 'trans-render/createTemplate.js';
 import {templStampSym} from 'trans-render/plugins/templStamp.js';
-import 'xtal-side-nav/xtal-side-nav.js';
 import '@ui5/webcomponents/dist/Link.js';
+import '@ui5/webcomponents-fiori/dist/SideNavigation.js';
+import '@ui5/webcomponents-fiori/dist/SideNavigationItem.js';
+import '@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js';
 //import 'p-et-alia/p-d.js';
 
 const mainTemplate = createTemplate(/* html */`
@@ -13,11 +15,34 @@ iframe{
 }
 </style>
 <slot name=linkList part=linksSlot style=display:none></slot>
-<xtal-side-nav>
+<ui5-shellbar
+primary-title="UI5 Web Components"
+secondary-title="The Best Run SAP"
+show-co-pilot
+>
+	<ui5-button icon="menu" slot="startButton" id="startButton"></ui5-button>
+</ui5-shellbar>
+
+<ui5-side-navigation>
+	<ui5-side-navigation-item text="Home" icon="home"></ui5-side-navigation-item>
+	<ui5-side-navigation-item text="People" expanded icon="group">
+		<ui5-side-navigation-sub-item text="From My Team"></ui5-side-navigation-sub-item>
+		<ui5-side-navigation-sub-item text="From Other Teams"></ui5-side-navigation-sub-item>
+	</ui5-side-navigation-item>
+	<ui5-side-navigation-item text="Locations" icon="locate-me" selected></ui5-side-navigation-item>
+	<ui5-side-navigation-item text="Events" icon="calendar">
+		<ui5-side-navigation-sub-item text="Local"></ui5-side-navigation-sub-item>
+		<ui5-side-navigation-sub-item text="Others"></ui5-side-navigation-sub-item>
+	</ui5-side-navigation-item>
+
+	<ui5-side-navigation-item slot="fixedItems" text="Useful Links" icon="chain-link"></ui5-side-navigation-item>
+	<ui5-side-navigation-item slot="fixedItems" text="History" icon="history"></ui5-side-navigation-item>
+</ui5-side-navigation>
+<!-- <xtal-side-nav>
 	<span slot=title>Catalog</span>
 	<div part=componentList>
 	</div>
-</xtal-side-nav>
+</xtal-side-nav> -->
 <iframe name=demoFrame></iframe>
 `);
 const uiRefs = {linksSlot: p, componentList: p};
